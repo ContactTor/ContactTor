6fd8ff932429038f825c698f826aeca2127bc0cb
Javid Mitchell <56163189+javidt28@users.noreply.github.com>
2020-10-05 21:53:48 -0400

    Updated the UI and added Signup button

--- app/src/main/java/com/example/instagram/LoginActivity.java
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -21,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
-
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +35,8 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,6 +44,14 @@ public class LoginActivity extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+//            ParseUser.signUpInBackground(username, password, new signUpInBackground() {}
+                @Override
+            public void onClick(View v) {
+
             }
         });
     }
